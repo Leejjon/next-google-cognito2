cfd3a7b6ea9ff51815fb0dc16880f11daf9d04b1
Leon Liefting <leon.liefting@persgroep.net>
2021-08-19 11:40:31 +0200

    Let's see if it works live.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,37 @@
 import '../styles/globals.css'
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+const isLocalhost = true;
+
+const [
+  localRedirectSignIn,
+  productionRedirectSignIn,
+] = awsconfig.oauth.redirectSignIn.split(",");
+
+const [
+  localRedirectSignOut,
+  productionRedirectSignOut,
+] = awsconfig.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = awsconfig;
+// const updatedAwsConfig = {
+//   ...awsconfig,
+//   oauth: {
+//     ...awsconfig.oauth,
+//     redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+//     redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+//   }
+// }
+
+console.log(JSON.stringify(updatedAwsConfig.oauth))
+
+Amplify.configure({
+  ...updatedAwsConfig,
+  ssr: true}
+);
+
+Auth.configure(updatedAwsConfig);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
